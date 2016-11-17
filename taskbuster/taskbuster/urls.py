21794c6428aa3b16a9d1b025f9d2c1f8b1f8f486
Maksim Dzhangirov <darkstill@ukr.net>
2016-11-18 01:17:23 +0200

    Editing urls

--- taskbuster/taskbuster/urls.py
+++ taskbuster/taskbuster/urls.py
@@ -21,7 +21,11 @@ from .views import home, home_files
 
 urlpatterns = [
     url(r'^(?P<filename>(robots.txt)|(humans.txt))$', home_files, name='home-files'),
+    url(r'^accounts/', include('allauth.urls')),
+    url(r'i18n/', include('django.conf.urls.i18n')),
 ]
+
+
 
 urlpatterns += i18n_patterns(
     url(r'^$', home, name='home'),
