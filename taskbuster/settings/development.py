dc27434976c71b97f5c1691323f01b029936b05c
Maksim Dzhangirov <darkstill@ukr.net>
2016-11-18 01:16:58 +0200

    Editing settings

--- taskbuster/settings/development.py
+++ taskbuster/settings/development.py
@@ -1,3 +1,11 @@
 # -*- coding: utf-8 -*-
 from .base import *
 DEBUG = True
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'taskbuster_db',
+        'USER': 'maksim',
+        'PASSWORD': '123456',
+    }
+}
